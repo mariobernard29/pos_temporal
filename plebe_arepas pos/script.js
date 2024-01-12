a780b501a37726e33075ff44f0d2163e112a897f
Mario Bernard <95982886+mariobernard29@users.noreply.github.com>
2024-01-12 01:47:56 -0700

    Add files via upload

---
+++ plebe_arepas pos/script.js
@@ -0,0 +1,102 @@
+$(document).ready(function() { 
+
+    var items = [];
+    $("#item-form").on("submit", addItemToCart);
+    $("#cart-table").on("click",".btn-danger", removeItemFromCart);
+    $("#generate-invoice").on("click", generateInvoice);
+
+
+
+    function addItemToCart(event) {
+        event.preventDefault();
+
+        var itemName = $("#item-name").val();
+        var itemPrice = $("#item-price").val();
+
+        if (itemName !== "" && itemPrice !== "") {
+            var item = {
+                name: itemName,
+                price: parseFloat(itemPrice),
+             };
+
+
+            items.push(item);
+            $("#cart-table tbody").append(
+              "<tr><td>" + item.name +"</td><td>$" + item.price.toFixed(2) + 
+              '</td><td><button class="btn btn-sm btn-danger"><i class="fa-solid fa-trash"></i></button></td></tr>');
+
+              updateTotalCost();
+              $("#item-name").val("");
+              $("#item-price").val("");
+            }
+        }   
+        
+        function removeItemFromCart() {
+            var index = $(this).closest("tr").index();
+            items.slice(index, 1);
+            $(this).closest("tr").remove();
+            updateTotalCost();
+        }
+
+        function updateTotalCost() {
+            var totalCost = 0;
+            items.forEach(function(item) {
+                totalCost += item.price;
+            });
+            $("#total-cost").text("Total: $" + totalCost.toFixed(2));
+        }
+
+        function generateInvoice() {
+            var invoice = `<html>
+            <head>
+                
+                <title>iNVOICE</title>
+                 <!-- Boostrap -->
+                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+                 integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+                 crossorigin="anonymous">
+            </head>
+            <body>
+                    <div class="container mt-5">
+                        <h1 class="text-center"><img src="logo.png" alt="" width="150px"></h1>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                </tr>
+                            </thead>
+            
+                            <tbody>`;
+
+            items.forEach(function(item) {
+                invoice +=
+                 "<tr><td>" +
+                  item.name + 
+                  "</td><td>$" +
+                item.price.toFixed(2) + 
+                "</td></tr>";
+            });
+
+            invoice += 
+            '</tbody></table><p class="text-right>Total Cost: $' +
+            getTotalCost() +
+             "</p></div></body></html>";
+
+            var popup = window.open("", "_blank");
+            popup.document.open();
+            popup.document.write(invoice);
+            popup.document.close();
+        }
+
+        function getTotalCost() {
+            var totalCost = 0;
+            items.forEach(function (item) {
+                totalCost += item.price;
+            });
+            return totalCost.toFixed(2);
+                        
+              
+   }       
+
+});
